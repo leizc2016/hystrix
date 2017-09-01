4e67ea5d6e1438c1bd2347d70d91246e42d5696b
leizhicheng <leizhicheng@lvmama.com>
2017-09-01 18:38:13 +0800

    spring mvc

--- src/main/java/com/lzc/controller/DemoController.java
+++ src/main/java/com/lzc/controller/DemoController.java
@@ -8,13 +8,18 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.lzc.filter.MethodHystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 //import com.lzc.accout.LoginService;
 
 @Controller
 @RequestMapping("/demo/")
 public class DemoController {
-
+	private static int succ =0;
+	private static Byte lock = new Byte("1");
+	
+	
 	@RequestMapping("main")
 	public String main() throws InterruptedException {
 		Thread.sleep(5000);
@@ -33,5 +38,38 @@ public class DemoController {
 		return "main";
 
 	}
+	
+	
+	@RequestMapping("anno")
+	@HystrixCommand(fallbackMethod = "okFallback", commandProperties = {
+			@HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="2000"),
+			@HystrixProperty(name="circuitBreaker.requestVolumeThreshold",value="5")
+	})
+	public String anno(HttpServletRequest request, HttpServletResponse response) throws InterruptedException {
+		Thread.sleep(1000);
+		long id=Thread.currentThread().getId();
+		System.out.println("vvv 成功了 ["+id+" ]");
+		synchronized (lock) {
+			succ++;
+			System.out.println(succ);
+		}
+		
+		return "main";
+
+	}
+	
+	protected Object getFallback() {
+		long id=Thread.currentThread().getId();
+		System.out.println("失败了 ["+id+" ]");
+		return null;
+	}
+	
+	public String okFallback(HttpServletRequest req,HttpServletResponse rep) {  
+        System.out.println("execute okFallback!");  
+        return "fallback";  
+    }
+    public String okFallback() {
+        return "fallbackssssss";
+    }
 
 }
